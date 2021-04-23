12e561dac52129980e0a7ba2bfe5247832a8a759
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-04-23 12:57:43 +0530

    Initial Commit

---
+++ prac.py
@@ -0,0 +1,39 @@
+class Creature():
+    """Create a simple Tomogachi Clone"""
+
+    def __init__(self, name):
+        """Initialise attributes"""
+        self.name = name.title()
+
+        # Attributes to track while playing the game.
+        self.hunger = 0
+        self.boredom = 0
+        self.tiredness = 0
+        self.dirtyness = 0
+
+    def eat():
+        pass
+
+    def play():
+        pass
+
+    def sleep():
+        pass
+
+    def awake():
+        pass
+
+    def clean():
+        pass
+
+    def forage():
+        pass
+
+    def show_values():
+        pass
+
+    def increment_values():
+        pass
+
+    def kill():
+        pass
